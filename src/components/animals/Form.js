e6ebf5dd8a92fbecbc2b77b55bf47a0be7d337b5
neyderF <49882090+neyderF@users.noreply.github.com>
2021-11-02 22:22:57 -0500

    image picker

--- src/components/animals/Form.js
+++ src/components/animals/Form.js
@@ -1,4 +1,4 @@
-import { Paper, Container, Button, Grid, Box, TextField } from '@mui/material'
+import { Paper, Container, Button, Grid, Box, TextField, IconButton, Chip } from '@mui/material'
 import React, { useState } from 'react'
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
@@ -8,24 +8,47 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
-
+import ImageUploading from 'react-images-uploading';
 import DateAdapter from '@mui/lab/AdapterMoment';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import { BiTrashAlt } from 'react-icons/bi';
 const steps = [
     'Características',
     'Vacunas',
 ];
 
+
 export default function Form() {
 
     const [activeStep, setActiveStep] = useState(0);
 
+
+    const [images, setImages] = React.useState([]);
+    const maxNumber = 8;
+
+    const onChange = (imageList, addUpdateIndex) => {
+        // data for submit
+        console.log(imageList, addUpdateIndex);
+        setImages(imageList);
+    };
+    
     const nextStep = () => {
         if (activeStep !== 2) {
             setActiveStep(activeStep + 1);
         }
     }
+
+
+    const toBack = () => {
+        if (activeStep > 0) {
+            setActiveStep(activeStep - 1);
+        }
+    }
+
 
     return (
         <Container maxWidth="sm" sx={{ marginBottom: 5, marginTop: 5 }}>
@@ -81,9 +104,12 @@ export default function Form() {
                                 <FormControlLabel value="macho" control={<Radio />} label="Hembra" />
                             </RadioGroup>
                         </FormControl>
+
                     </Grid>
+
+
                     <Grid item xs={12} md={6}
-                        justifyContent="space-between"
+                        justifyContent="flex-start"
                         flexDirection="column"
                         display="flex"
                         padding={3}
@@ -92,27 +118,203 @@ export default function Form() {
                             <DatePicker
                                 label="Fecha de nacimiento (aproximada)"
                                 // value={value}
-                                // onChange={(newValue) => {
-                                //     setValue(newValue);
-                                // }}
+                                onChange={(newValue) => {
+                                    console.log(newValue)
+                                }}
                                 renderInput={(params) => <TextField {...params} variant="standard" />}
                             />
                         </LocalizationProvider>
-                        <TextField disabled label="Edad calculada" value="10 meses" variant="filled" />
+                        <TextField disabled sx={{ marginTop: 3 }} label="Edad calculada" value="10 meses" variant="filled" />
+                    </Grid>
+                    <Grid item md={6}
+                        xs={12}
+                        justifyContent="center"
+                        // alignItems="center"
+                        display="flex"
+                        flexDirection="column"
+                        padding={3}
+                    >
+                        <FormControl component="fieldset" sx={{ marginTop: 2 }}>
+                            <FormLabel component="label" sx={{ fontSize: 14 }}>Especie</FormLabel>
+                            <RadioGroup row aria-label="specie" name="row-radio-buttons-group">
+                                <FormControlLabel value="perro" control={<Radio />} label="Perro" />
+                                <FormControlLabel value="gato" control={<Radio />} label="Gato" />
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item md={6}
+                        xs={12}
+                        justifyContent="center"
+                        // alignItems="center"
+                        display="flex"
+                        flexDirection="column"
+                        padding={3}
+                    >
+                        <TextField
+                            fullWidth
+                            placeholder="grande, mediano o pequeño"
+                            // error={errors.name}
+                            label="Tamaño"
+                            // helperText={errors.name}
+                            variant="standard"
+                        // value={values.name}
+                        // onChange={(event) => {
+                        //     setValues({ ...values, ["name"]: event.target.value });
+                        //     setErrors({ ...errors, ["name"]: null })
+                        // }}
+                        />
                     </Grid>
                     <Grid item xs={12} md={12} padding={3}>
-                        <TextField label="Descripción" fullWidth multiline={true} minRows={8} variant="filled" />
+                        <TextField label="Descripción" fullWidth multiline={true} minRows={4} variant="filled" />
 
                     </Grid>
-                    {/* <Grid item xs={6} md={8}>
-                        <p>hola</p>
 
-                    </Grid> */}
-                </Grid> : null}
+                </Grid> :
+
+                    <Grid container >
+                        <Grid item md={6}
+                            xs={12}
+                            justifyContent="center"
+                            // alignItems="center"
+                            display="flex"
+                            flexDirection="column"
+                            padding={3}
+                        >
+
+                            <TextField
+                                fullWidth
+                                // error={errors.name}
+                                label="Sitio de rescate"
+                                // helperText={errors.name}
+                                variant="standard"
+                            // value={values.name}
+                            // onChange={(event) => {
+                            //     setValues({ ...values, ["name"]: event.target.value });
+                            //     setErrors({ ...errors, ["name"]: null })
+                            // }}
+                            />
+
+                        </Grid>
 
 
+                        <Grid item xs={12} md={6}
+                            justifyContent="flex-start"
+                            flexDirection="column"
+                            display="flex"
+                            padding={3}
+                        >
+                            <LocalizationProvider dateAdapter={DateAdapter}>
+                                <DatePicker
+                                    label="Fecha de rescate (aproximada)"
+                                    // value={value}
+                                    onChange={(newValue) => {
+                                        console.log(newValue)
+                                    }}
+                                    renderInput={(params) => <TextField {...params} variant="standard" />}
+                                />
+                            </LocalizationProvider>
+                        </Grid>
+                        <Grid item md={12}
+                            xs={12}
+                            justifyContent="center"
+                            alignItems="center"
+                            display="flex"
+                            flexDirection="column"
+                        // padding={3}
+                        >
+                            <FormControl component="fieldset" sx={{ marginTop: 2 }}>
+                                <FormLabel component="label" sx={{ fontSize: 14 }}>Estado</FormLabel>
+                                <RadioGroup row aria-label="state" name="row-radio-buttons-group">
+                                    <FormControlLabel value="esterilizado" control={<Radio />} label="Esterilizado" />
+                                    <FormControlLabel value="desparasitado" control={<Radio />} label="Desparasitado" />
+                                </RadioGroup>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12} md={12} padding={3}>
+                            <TextField label="Vacunas" fullWidth multiline={true} minRows={4} variant="filled" />
+
+                        </Grid>
+                    </Grid>
+
+                }
+
+                {activeStep === 0 &&
+
+
+                    <ImageUploading
+                        multiple
+                        value={images}
+                        onChange={onChange}
+                        maxNumber={maxNumber}
+                        dataURLKey="data_url"
+                        maxFileSize={5000000}
+                        acceptType={['jpg', 'png']}
+                    >
+                        {({
+                            imageList,
+                            onImageUpload,
+                            onImageRemoveAll,
+                            onImageUpdate,
+                            onImageRemove,
+                            isDragging,
+                            dragProps,
+                            errors
+                        }) => (
+                            // write your building UI
+                            <div style={{ display: "flex", alignItems: "center", flexDirection: "column", backgroundColor: "#ededed", padding: 5, borderRadius: 3, margin: 22 }}>
+
+
+
+
+                                {imageList.length !== 0 && false ? <Button size="small" onClick={onImageRemoveAll} variant="contained" color="info">Eliminar Todo</Button> : null}
+
+                                <Button onClick={onImageUpload} size="small" sx={{ marginTop: 2 }} variant="contained" color={isDragging ? "info" : "primary"}>Seleccionar imagenes</Button>
+                                <Chip sx={{ marginTop: 2 }} label={imageList.length + "/8"} />
+                                {errors && <div>
+                                    {errors.maxNumber && <span>Solo puede adjuntar un máximo de {maxNumber} imagenes por animal</span>}
+                                    {errors.acceptType && <span>Este tipo de archivo no está soportado</span>}
+                                    {errors.maxFileSize && <span>Cada imágen debe pesar máximo 5Mb</span>}
+
+                                </div>}
+                                <div style={{ marginTop: imageList.length > 0 ? 15 : 0, display: "flex" }}>
+
+                                    <ImageList sx={{ width: "100%", maxHeight: 250 }}>
+                                        {imageList.map((image, index) => (
+                                            <ImageListItem key={index}>
+                                                <img
+                                                    src={image['data_url']}
+                                                    // srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                                    // alt={item.title}
+                                                    loading="lazy"
+                                                />
+                                                <ImageListItemBar
+                                                    // title={image["file"].name}
+                                                    subtitle={image["file"].name}
+                                                    actionIcon={
+                                                        <IconButton
+                                                            sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                                            aria-label={`info about this`}
+                                                            onClick={() => onImageRemove(index)}
+                                                        >
+                                                            <BiTrashAlt />
+                                                        </IconButton>
+                                                    }
+                                                />
+                                            </ImageListItem>
+                                        ))}
+                                    </ImageList>
+                                </div>
+                            </div>
+                        )}
+                    </ImageUploading>
+
+
+                }
                 <Box sx={{ justifyContent: "center", paddingBottom: 3 }} display="flex">
-                    <Button size="small" variant="contained" color="warning" onClick={() => nextStep()}>Siguiente</Button>
+                    {activeStep !== 0 && <Button size="medium" variant="contained" color="info" sx={{ marginRight: 2 }} onClick={() => toBack()}>Atrás</Button>}
+
+                    <Button size="medium" variant="contained" color="warning" onClick={() => nextStep()}>Siguiente</Button>
                 </Box>
             </Paper >
         </Container>
