3a4aaca712e14314e5653cd4cb65d9db2f0d9c2d
AndresLlinas <andreslli1707@gmail.com>
2021-11-11 18:40:28 -0500

    Footer changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,41 +1,56 @@
-import React from "react"
+import React from 'react'
+import './footer.scss'
 
-const Footer = () => <footer className="page-footer font-small blue pt-4">
+const Footer = () => (
+  <footer className="page-footer pt-4">
     <div className="container-fluid text-center text-md-left">
-        <div className="row">
-            <div className="col-md-6 mt-md-0 mt-3">
-                <h5 className="text-uppercase">Footer Content</h5>
-                <p>Here you can use rows and columns to organize your footer content.</p>
-            </div>
-
-            <hr className="clearfix w-100 d-md-none pb-0"/>
-
-            <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
-                <ul className="list-unstyled">
-                    <li><a href="#!">Link 1</a></li>
-                    <li><a href="#!">Link 2</a></li>
-                    <li><a href="#!">Link 3</a></li>
-                    <li><a href="#!">Link 4</a></li>
-                </ul>
-            </div>
-
-            <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
-                <ul className="list-unstyled">
-                    <li><a href="#!">Link 1</a></li>
-                    <li><a href="#!">Link 2</a></li>
-                    <li><a href="#!">Link 3</a></li>
-                    <li><a href="#!">Link 4</a></li>
-                </ul>
-            </div>
+      <div className="row">
+        <div className="col-md-6 mt-md-0 mt-3 footer-text">
+          <img src={'/images/logotipo.png'} alt="" width="250" />
+          <p className="pt-4">Fundación Fundamor de Calarcá.</p>
         </div>
+
+        <hr className="clearfix w-100 d-md-none pb-0" />
+
+        <div className="col-md-3 mb-md-0 mb-3">
+          <h5 className="text-uppercase">Servicios</h5>
+          <ul className="list-unstyled footer-links">
+            <li>
+              <a href="#!">Adopta</a>
+            </li>
+            <li>
+              <a href="#!">Acerca de</a>
+            </li>
+            <li>
+              <a href="#!">Contacto</a>
+            </li>
+            <li>
+              <a href="#!">Ingresar</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-md-3 mb-md-0 mb-3">
+          <h5 className="text-uppercase">Siguenos en</h5>
+          <ul className="list-unstyled footer-links">
+            <li>
+              <a href="#!">Facebook</a>
+            </li>
+            <li>
+              <a href="#!">Instagram</a>
+            </li>
+            <li>
+              <a href="#!">Youtube</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
 
-    <div className="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+    <div className="footer-copyright text-center py-3 p">
+      © 2021 Copyright: Fundamor Calarcá
     </div>
-
-</footer>
+  </footer>
+)
 
 export default Footer
