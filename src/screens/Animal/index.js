518151f285455f4427305864fc7dcf969fe41fd6
neyderF <49882090+neyderF@users.noreply.github.com>
2021-12-22 21:59:41 -0500

    lista de animales, poner en tablas las grandes cantidades de datos
    poner en tablas las grandes cantidades de datos, como la lista de animales, adopciones y adoptantes , buscar data grid

--- src/screens/Animal/index.js
+++ src/screens/Animal/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Form from '../../components/animals/Form'
 import { useRouteMatch, Switch, Route } from "react-router-dom";
 import FormEdit from '../../components/animals/FormEdit';
+import List from '../../components/animals/List';
 export default function Animal() {
 
     let { path } = useRouteMatch();
     return <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         <Switch>
             <Route exact path={path}>
+                <List />
+            </Route>
+            <Route  path={`${path}/new`}>
                 <Form />
             </Route>
             <Route path={`${path}/edit/:animalId`}>
