54fcb709ccd189bf269c928dfac7dded826d332c
AndresLlinas <andreslli1707@gmail.com>
2022-01-15 13:22:00 -0500

    Revert "shared route typo"
    This reverts commit a11d03d4739f0870733796d34f2e8359d09179eb.

--- src/context/animal/animalState.js
+++ src/context/animal/animalState.js
@@ -3,7 +3,7 @@ import AnimalContext from './animalContext';
 import AnimalReducer from './animalReducer';
 import { ANIMALS, SELECT_ANIMAL, TOGGLE_ANIMAL_LOADING, ANIMAL_MESSAGE } from '../../types';
 import axiosClient from '../../config/axios';
-import { handleResponseError } from '../../shared/utils';
+import { handleResponseError } from '../../Shared/utils';
 
 /**gestionar codigos de errores y timeout
  * 
